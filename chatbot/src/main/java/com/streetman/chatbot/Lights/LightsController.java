c006d8d3d8c679b9e5d7422ccc0803b038b45bf0
Akhilesh <akhilesh.2101005@srec.ac.in>
2024-10-16 13:50:40 +0530

    New Change for Light

--- chatbot/src/main/java/com/streetman/chatbot/Lights/LightsController.java
+++ chatbot/src/main/java/com/streetman/chatbot/Lights/LightsController.java
@@ -1,4 +1,59 @@
 package com.streetman.chatbot.Lights;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lights")
 public class LightsController {
+
+    @Autowired
+    private LightsService lightsService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Lights> save(@RequestBody Lights lights)
+    {
+        Lights saveLights = lightsService.createLights(lights);
+        return ResponseEntity.ok(saveLights);
+
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Lights>> getAllLights()
+    {
+        List<Lights> lights = lightsService.getAllLights();
+        return  ResponseEntity.ok(lights);
+    }
+
+    @GetMapping("/list/{lightid}")
+    public ResponseEntity<Lights> getLightsById(@PathVariable Long lightid)
+    {
+        Lights lights = lightsService.getLightById(lightid);
+        return ResponseEntity.ok(lights);
+    }
+
+    @PutMapping("/change/{id}")
+    public ResponseEntity<Lights> updateLights(@PathVariable("id") Long lightid ,@RequestBody Lights lights)
+    {
+       return  ResponseEntity.ok(lightsService.updateLights(lightid,lights));
+    }
+
+    @DeleteMapping("/remove/{id}")
+    private ResponseEntity<Void> deleteZone(@PathVariable("id") Long ligthid)
+    {
+        try {
+
+            lightsService.deleteByScheduleId(ligthid);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- chatbot/src/main/java/com/streetman/chatbot/Lights/LightsService.java
+++ chatbot/src/main/java/com/streetman/chatbot/Lights/LightsService.java
@@ -1,4 +1,58 @@
 package com.streetman.chatbot.Lights;
 
+
+import com.streetman.chatbot.scheduling.Schedules;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LightsService {
+
+    @Autowired
+    private LightsRepository lightsRepository;
+
+    //POST
+    public Lights createLights(Lights lights)
+    {
+        return lightsRepository.save(lights);
+    }
+
+    //GET
+
+    public List<Lights> getAllLights()
+    {
+        return lightsRepository.findAll();
+    }
+
+    //GET
+
+    public  Lights getLightById(Long lightid)
+    {
+        return lightsRepository.findById(lightid).orElseThrow();
+    }
+
+    //PUT
+
+    public Lights updateLights(Long lightid,Lights lightsDetails)
+    {
+        Lights lights = getLightById(lightid);
+
+        lights.setSerialNumber(lightsDetails.getSerialNumber());
+        lights.setModel(lightsDetails.getModel());
+        lights.setLightstate(lightsDetails.getLightstate());
+        lights.setLightstate(lightsDetails.getLightstate());
+        lights.setLightlevel(lightsDetails.getLightlevel());
+
+        return lightsRepository.save(lights);
+    }
+
+    //Delete
+
+    public void deleteByScheduleId(Long lightid)
+    {
+        Lights lights = lightsRepository.findById(lightid).orElseThrow();
+        lightsRepository.delete(lights);
+    }
 }
